c0ebb6b847f4cdffbf269b7681a9342e086c5198
Bhanjo <hanjo1515@naver.com>
2020-05-04 16:51:27 +0900

    0504

---
+++ Slide.js
@@ -0,0 +1,54 @@
+let index = 0; //포트폴리오 이미지 목록
+let indexTodo = 0; //스케쥴 이미지 목록
+let indexLotto = 0; //로또 이미지 목록
+
+window.onload = function() {
+    slideShowPortfolio();
+    slideShowTodo();
+    slideShowLotto();
+}
+
+//포트폴리오 슬라이드
+function slideShowPortfolio() {
+    let i;
+    let x = document.getElementsByClassName("projectImg")
+    for(i = 0; i<x.length; i++){
+        x[i].style.display="none";
+    }
+    index++;
+    if(index > x.length){
+        index = 1;
+    }
+    x[index-1].style.display = "inline";
+    setTimeout(slideShowPortfolio, 3000);
+}
+
+ //스케쥴 슬라이드
+function slideShowTodo() {
+    let j;
+    let y = document.getElementsByClassName("projectImgTodo")
+    for(j = 0; j<y.length; j++){
+        y[j].style.display="none";
+    }
+    indexTodo++;
+    if(indexTodo > y.length){
+        indexTodo = 1;
+    }
+    y[indexTodo-1].style.display = "inline";
+    setTimeout(slideShowTodo, 3000);
+}
+
+//로또 슬라이드
+/*function slideShowLotto() {
+    let k;
+    let z = document.getElementsByClassName("projectImgLotto")
+    for(k = 0; k<z.length; k++){
+        z[k].style.display="none";
+    }
+    indexLotto++;
+    if(indexLotto > z.length){
+        indexLotto = 1;
+    }
+    z[indexLotto-1].style.display = "inline";
+    setTimeout(slideShowTodo, 3000);
+}*/
